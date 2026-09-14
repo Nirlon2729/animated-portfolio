f3ca8937d9dbea15e6231ccec3385b31246f699c
Nirlon2729 <nirlonmacwan27@gmail.com>
2026-09-14 14:23:22 +0530

    Replace certities

--- src/components/CertificatePage.jsx
+++ src/components/CertificatePage.jsx
@@ -5,14 +5,51 @@ import cert2 from "../assets/image/certi/Coursera2.jpg";
 import cert3 from "../assets/image/certi/certi_1.png";
 import cert4 from "../assets/image/certi/certi_2.png";
 import cert5 from "../assets/image/certi/certi_3.png";
-
+const certi1 = "/certificates/ibm_1.png";
+const certi2 = "/certificates/ibm_2.png";
 const certificates = [
     {
         id: "01",
+        title: "Career Management Essentials",
+        issuer: "IBM",
+        date: "September 2026",
+        image: certi1,
+        link: "https://www.credly.com/badges/ee8e0d69-e688-45a2-aa44-a9788fd020b0",
+        description:
+            "IBM SkillsBuild – Career Management Essentials: Completed a professional development program focused on career planning, professional branding, workplace readiness, and strategies for long-term career growth.",
+        skills: [
+            "Career Development",
+            "Career Management",
+            "Networking Skills",
+            "Communication Skills",
+            "Professional Online Brand Development",
+            "Workplace Research"
+        ]
+    },
+    {
+        id: "02",
+        title: "Python Basics: Selection and Iteration",
+        issuer: "Coursera",
+        date: "January 2025",
+        image: certi2,
+        link: "https://www.credly.com/badges/ee8e0d69-e688-45a2-aa44-a9788fd020b0",
+        description:
+            "Successfully completed a comprehensive course focused on Python fundamentals, decision making, loops, iterative programming and problem-solving techniques. Built a strong foundation in computational thinking and algorithmic design.",
+        skills: [
+            "Python",
+            "Programming Logic",
+            "Loops",
+            "Conditionals",
+            "Problem Solving"
+        ]
+    },
+    {
+        id: "03",
         title: "Python Basics: Selection and Iteration",
         issuer: "Coursera",
         date: "January 2025",
         image: cert1,
+        link: "",
         description:
             "Successfully completed a comprehensive course focused on Python fundamentals, decision making, loops, iterative programming and problem-solving techniques. Built a strong foundation in computational thinking and algorithmic design.",
         skills: [
@@ -25,11 +62,12 @@ const certificates = [
     },
 
     {
-        id: "02",
+        id: "04",
         title: "Python Structures: Lists, Strings & Files",
         issuer: "Coursera",
         date: "January 2025",
         image: cert2,
+        link: "",
         description:
             "Learned essential Python data structures and file handling concepts. Developed practical skills for working with strings, lists, text processing and real-world data management tasks.",
         skills: [
@@ -42,12 +80,12 @@ const certificates = [
     },
 
     {
-        id: "03",
+        id: "05",
         title: "Computer Concepts Course (CCC)",
         issuer: "JanKalyan Computer Saksharta Mission",
         date: "July 2023",
         image: cert3,
-
+        link: "",
         description:
             "Successfully completed the Certificate Course in Computer Concepts (CCC), gaining a strong foundation in computer operations, digital literacy, office productivity tools, and internet technologies. The program focused on practical computer usage required in academic, professional, and everyday digital environments.",
 
@@ -71,12 +109,12 @@ const certificates = [
     },
 
     {
-        id: "04",
+        id: "06",
         title: "Programming in C",
         issuer: "JanKalyan Computer Saksharta Mission",
         date: "July 2023",
         image: cert4,
-
+        link: "",
         description:
             "Completed a comprehensive introductory programming course in C language, focusing on structured programming, logical thinking, and problem-solving. The course provided hands-on experience in writing, debugging, and optimizing programs while developing a strong understanding of core programming concepts.",
 
@@ -100,11 +138,12 @@ const certificates = [
     },
 
     {
-        id: "05",
+        id: "07",
         title: "Kaushal Nidhi E-Shiksha Yojana",
         issuer: "NIED Foundation",
         date: "July 2023",
         image: cert5,
+        link: "",
         description:
             "Completed a skill development initiative focused on digital literacy, computer operations and foundational technical knowledge, earning Grade A certification.",
         skills: [
@@ -153,7 +192,7 @@ function CertificatePage() {
 
             <div className="certStats">
 
-                
+
 
             </div>
 
@@ -185,6 +224,16 @@ function CertificatePage() {
                                 <span> {cert.date}</span>
                                 <span>🏆 Certified</span>
                             </div>
+                            {cert.link && (
+                                <a
+                                    href={cert.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="certVerifyBtn"
+                                >
+                                🔗 View Credential / Verfy Certificate 
+                                </a>
+                            )}
                             <div className="certHighlights">
                                 {cert.highlights?.map((item) => (
                                     <div key={item} className="certHighlight">
